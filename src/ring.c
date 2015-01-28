ce68dcf00b6a5769767616a45c3713b4ede1fd4d
xoozi <wwfxtt@gmail.com>
2015-01-28 10:34:15 +0800

    add sparse array

--- src/ring.c
+++ src/ring.c
@@ -14,14 +14,18 @@ struct T{
         struct node *llink, *rlink;
         void *value;
     }*head;
-    
-    ssize_t length;
+
+    ssize_t     lastget_pos;
+    struct node *lastget_node;
+    ssize_t     length;
 };
 
 
 static struct node* ring_node(T ring, ssize_t position);
 
 static void ring_node_delete(T ring, struct node *p);
+
+static inline void reset_lastget(T ring);
 
 T
 ring_new(void)
@@ -89,8 +93,17 @@ ring_get(T ring, ssize_t position)
     assert(ring);
     assert(position >=0 && position < ring->length);
 
-    q = ring_node(ring, position);
+    if(position == (ring->lastget_pos + 1)){
+        q = ring->lastget_node->rlink;
+    }
+    else if(position == (ring->lastget_pos - 1)){
+        q = ring->lastget_node->llink;
+    }else{
+        q = ring_node(ring, position);
+    }
 
+    ring->lastget_pos = position;
+    ring->lastget_node = q;
     return q->value;
 }
 
@@ -122,6 +135,7 @@ ring_add(T ring, ssize_t position, void *x)
     assert(ring);
     assert(position >= -ring->length && position <= ring->length+1);
 
+
     if(1 == position || position == -ring->length)
         return ring_add_low(ring, x);
     else if(0 == position || position == ring->length+1)
@@ -137,6 +151,8 @@ ring_add(T ring, ssize_t position, void *x)
         q->llink = p;
 
         ring->length++;
+
+        reset_lastget(ring);
         return p->value = x;
     }
 }
@@ -148,6 +164,8 @@ ring_add_low(T ring, void *x)
     assert(ring);
     ring_add_high(ring, x);
     ring->head = ring->head->llink;
+
+    reset_lastget(ring);
     return x;
 }
 
@@ -170,6 +188,8 @@ ring_add_high(T ring, void *x)
     }
 
     ring->length++;
+
+    reset_lastget(ring);
 
     return p->value = x;
 }
@@ -194,6 +214,7 @@ ring_remove(T ring, ssize_t position)
     
     ring_node_delete(ring, q);
 
+    reset_lastget(ring);
     return x;
 }
 
@@ -204,6 +225,7 @@ ring_remove_low(T ring)
     assert(ring);
     assert(ring->length > 0);
     ring->head = ring->head->rlink;
+    reset_lastget(ring);
     return ring_remove_high(ring);
 }
 
@@ -222,6 +244,7 @@ ring_remove_high(T ring)
 
     ring_node_delete(ring, q); 
 
+    reset_lastget(ring);
     return x;
 }
 
@@ -242,6 +265,8 @@ ring_rotate(T ring, ssize_t n)
     q = ring_node(ring, position);
 
     ring->head = q;
+
+    reset_lastget(ring);
 }
 
 
@@ -273,4 +298,13 @@ ring_node_delete(T ring, struct node *p)
     FREE(p);
     if(--ring->length == 0)
         ring->head = NULL;
+}
+
+static 
+inline 
+void 
+reset_lastget(T ring)
+{
+    ring->lastget_node = ring->head;
+    ring->lastget_pos = 0;
 }
